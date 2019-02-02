f419de7486d84e927bf26f48a395f51c0c2eb43e
Samuel Borges <samuelbfg@gmail.com>
2019-02-02 17:59:34 -0300

    Create MissionToMars Project

---
+++ MissionToMars/src/com/company/Rocket.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Rocket implements SpaceShip{
+
+    public boolean launch(){
+        return true;
+    }
+
+    public boolean land(){
+        return true;
+    }
+
+    public
+
+}
