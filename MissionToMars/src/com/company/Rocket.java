a9abf28ed127e7d22232364c810abfa2379ec388
Samuel Borges <samuelbfg@gmail.com>
2019-02-02 18:16:31 -0300

    SpaceShip Interface and Rocket Class

--- MissionToMars/src/com/company/Rocket.java
+++ MissionToMars/src/com/company/Rocket.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Rocket implements SpaceShip{
 
+    int rocketCost;
+    int rocketWeight;
+    int maxRocketWeigth; // Max weight with cargo
+    int cargoCarried;
+    int rocketCurrentWeight;
+
+    double explosionRate;
+    double crashingRate;
+
+
+
     public boolean launch(){
+
         return true;
     }
 
@@ -10,6 +22,16 @@ public class Rocket implements SpaceShip{
         return true;
     }
 
-    public
+    public boolean canCarry(Item item){
+        if(item.weight + this.rocketCurrentWeight <= maxRocketWeigth){
+            return true;
+        }
+        return false;
+    }
+
+    public void carry(Item item){
+        this.rocketCurrentWeight = this.rocketCurrentWeight + item.weight;
+        this.cargoCarried = this.rocketCurrentWeight - this. rocketWeight;
+    }
 
 }
